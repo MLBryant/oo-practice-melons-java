26300b8a7233f2303fb47c58bc13cd8a2e0143eb
Mason Bryant <mlbryant6@gmail.com>
2022-01-03 00:45:23 -0600

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Instantiate objects here!");
+
+		MelonType muskmelon = new MelonType("musk", "muskmelon", 1998, "green", true, true);
+
+		MelonType casaba = new MelonType("cas", "casaba", 2003, "orange", false, false);
+
+		MelonType crenshaw = new MelonType("cren", "crenshaw", 1996, "green", false, false);
+
+		MelonType yellowWatermelon = new MelonType("yw", "yellow watermelon", 2013, "yellow", false, true);
+	}
+
+}
