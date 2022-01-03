b809f27b0d02af297efdf81f3fc0b4dc7f47890d
Mason Bryant <mlbryant6@gmail.com>
2022-01-03 01:33:53 -0600

    completed exercise

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import static java.lang.String.format;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -10,6 +12,37 @@ public class Main {
 		MelonType crenshaw = new MelonType("cren", "crenshaw", 1996, "green", false, false);
 
 		MelonType yellowWatermelon = new MelonType("yw", "yellow watermelon", 2013, "yellow", false, true);
+
+		MelonType[] melonTypes = {muskmelon, casaba, crenshaw, yellowWatermelon};
+
+		Melon melon1 = new Melon(melonTypes[3], 8, 7, 2, "Sheila");
+
+		Melon melon2 = new Melon(melonTypes[3], 3, 4, 2, "Sheila");
+
+		Melon melon3 = new Melon(melonTypes[1], 10, 6, 35, "Sheila");
+
+		Melon melon4 = new Melon(melonTypes[2], 8, 9, 35, "Michael");
+
+		Melon melon5 = new Melon(melonTypes[2], 8, 2, 35, "Michael");
+
+		Melon melon6 = new Melon(melonTypes[0], 7, 10, 3, "Michael");
+
+		Melon melon7 = new Melon(melonTypes[3], 7, 10, 3, "Sheila");
+
+		Melon[] melons = {melon1, melon2, melon3, melon4, melon5, melon6, melon7};
+
+
+		getSellabilityReport(melons);
+	}
+
+	public static void getSellabilityReport(Melon[] melons) {
+		for ( Melon melon: melons) {
+			if (melon.isSellable()) {
+				System.out.printf("Harvested by %s from Field %s (CAN BE SOLD)%n", melon.harvester, melon.field);
+			} else {
+				System.out.printf("Harvested by %s from Field %s (NOT SELLABLE)%n", melon.harvester, melon.field);
+			}
+		}
 	}
 
 }
